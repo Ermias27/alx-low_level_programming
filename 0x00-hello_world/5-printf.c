0eed52460bbc1a2fc064cbcecf3b65a85cbeeb18
Ermias Tesfaye <ermias.nigatu11@gmail.com>
2023-07-16 11:50:43 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that prints a line using the printf funciton
+ * return:0 on successful execution
+ */
+int main(void);
+
+{
+printf("with proper grammar, but the outcome is a piece of art,\n")
+return (0);
+return (0);
+}
+
